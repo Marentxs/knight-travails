5c22c1cb94402d026af0224bbda7260a2fba1c35
Julian <111262742+Marentxs@users.noreply.github.com>
2026-03-17 12:35:19 -0500

    add main function knightMoves, fixed issue with reconstruct

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-function reconstruct(start, end) {
-  let path = [];
-  const at = end;
+function reconstruct(parent, start, end) {
+  const path = [end];
+  let at = end;
+
   while (at != start) {
-    path.prepend(at);
     at = parent[at];
-    path.prepend(start);
-    return path;
+    path.unshift(at);
   }
+
+  return path;
 }
 
 function getNeighbors(current) {
@@ -31,4 +32,33 @@ function getNeighbors(current) {
     const [cx, cy] = candidate.split(",").map(Number);
     return cx >= 0 && cx <= 7 && cy >= 0 && cy <= 7;
   });
+}
+
+function knightMoves(start, end) {
+  if (start === end) {
+    return [start];
+  }
+
+  const queue = [start];
+  const visited = [start];
+  const parent = {};
+
+  while (queue.length !== 0) {
+    const current = queue.shift();
+
+    const neighbors = getNeighbors(current);
+    for (const neighbor of neighbors) {
+      if (!visited.includes(neighbor)) {
+        visited.push(neighbor);
+        parent[neighbor] = current;
+
+        if (neighbor === end) {
+          return reconstruct(parent, start, end);
+        }
+
+        queue.push(neighbor);
+      }
+    }
+  }
+  return "no path";
 }
